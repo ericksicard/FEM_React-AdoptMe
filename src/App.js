b90bb455d9f6ce1455355c7e1cae312961b1a29b
ericksicard <esrfree@gmail.com>
2021-03-13 18:24:51 -0500

    async calls to Petfinder API, update Pet component

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-    <div id="parent-element">
-      <h1 id="child-element">"Adopt Me!</h1>
-      <SearchParams/>
-    </div>
-  )
+    <React.StrictMode>
+      <div id="parent-element">
+        <h1 id="child-element">"Adopt Me!</h1>
+        <SearchParams/>
+      </div>
+    </React.StrictMode>
+  );
 };
 
 render(<App />, document.getElementById("root"));
